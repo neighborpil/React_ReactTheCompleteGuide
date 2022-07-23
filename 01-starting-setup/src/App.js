2df0ef480a2d7eed3b859b429d7677f0a4bdb781
Neighborpil <neighborpil@naver.com>
2022-07-23 17:46:45 +0900

    save

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,12 +1,23 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem />
-    </div>
-  );
+
+	const expenses = [
+		{ id: 'e1', title: 'Car Insurance', amount: 294.67, date: new Date(2022, 7, 23)},
+		{ id: 'e2', title: 'Car Insurance2', amount: 294.67, date: new Date(2022, 7, 23)},
+		{ id: 'e3', title: 'Car Insurance3', amount: 294.67, date: new Date(2022, 7, 23)},
+		{ id: 'e4', title: 'Car Insurance4', amount: 294.67, date: new Date(2022, 7, 23)},
+	]
+
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+        </div>
+    );
 }
 
 export default App;
