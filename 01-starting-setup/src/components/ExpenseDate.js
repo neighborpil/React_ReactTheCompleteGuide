15f7add7062c17e778d55f4291555080eb9d9b45
Neighborpil <neighborpil@naver.com>
2022-07-26 07:59:35 +0900

    save

---
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,20 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props) {
+
+	const month = props.date.toLocaleString('ko-KR', {month: 'long'}); 
+	const day= props.date.toLocaleString('ko-KR', {day: '2-digit'}); 
+	const year = props.date.getFullYear()
+
+
+    return (
+        <div className='expense-date'>
+            <div className='expense-date__year'>{year}</div>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__day'>{day}</div>
+			{/* <div>{props.date.toISOString()}</div> */}
+        </div>
+    )
+}
+
+export default ExpenseDate;
