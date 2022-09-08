f5f1295b33906a078076d6954fea4b80b53980ec
neighborpil <neighborpil@naver.com>
2022-09-08 13:11:15 +0900

    save

--- 11_Food_Order_App/src/components/Cart/Cart.js
+++ 11_Food_Order_App/src/components/Cart/Cart.js
@@ -12,12 +12,12 @@ const Cart = props => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemAddHandler = id => {
-
+    const cartItemAddHandler = item => {
+        cartCtx.addItem({...item, amount: 1});
     };
 
     const cartItemRemoveHandler = id => {
-
+        cartCtx.removeItem(id);
     };
 
     const cartItems = (
