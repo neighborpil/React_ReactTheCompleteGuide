693083caf3c42c4594b313de5de358a915540fd8
neighborpil <neighborpil@naver.com>
2022-09-08 17:01:20 +0900

    save

--- 12-look_behind_the_scenes_of_react/src/App.js
+++ 12-look_behind_the_scenes_of_react/src/App.js
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState();
+  const [allowToggle, setAllowToggle] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false);
 
   console.log('APP RUNNING');
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
+
+  const toggleParagraphHandler = useCallback(() => {
+    if(allowToggle) {
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+  }, [allowToggle]);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      { showParagraph && <p>This is new!</p> }
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
