f592cf3b50cc5995c80585c0bdf0507af4def23f
neighborpil <neighborpil@naver.com>
2022-12-26 10:09:42 +0900

    save

--- 18_diving_into_redux/src/store/index.js
+++ 18_diving_into_redux/src/store/index.js
@@ -3,31 +3,43 @@ import counter from "../components/Counter";
 
 // export const INCREMENT = 'increment';
 
-const initialState = {counter: 0, showCounter: true};
+const initialCounterState = {counter: 0, showCounter: true};
 
 const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
+    name: 'counter', initialState: initialCounterState, reducers: {
         increment(state) {
             state.counter++;
-        },
-        decrement(state) {
+        }, decrement(state) {
             state.counter--;
-        },
-        increase(state, action) {
-            state.counter == state.counter + action.payload;
-        },
-        toggleCounter(state) {
+        }, increase(state, action) {
+            state.counter = state.counter + action.payload;
+        }, toggleCounter(state) {
             state.showCounter = !state.showCounter;
         }
     }
 
 });
 
+const initialAuthState = {
+    isAuthenticated: false
+}
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        }, logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
+});
+
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer},
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
