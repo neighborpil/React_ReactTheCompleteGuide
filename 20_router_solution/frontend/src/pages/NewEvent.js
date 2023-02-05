83e4cccc84662d15dd0fc60722566f8b432eb499
Neighborpil <neighborpil@naver.com>
2023-02-05 16:35:51 +0900

    save

--- 20_router_solution/frontend/src/pages/NewEvent.js
+++ 20_router_solution/frontend/src/pages/NewEvent.js
@@ -1,4 +1,5 @@
 import EventForm from "../components/EventForm";
+import {json, redirect} from "react-router-dom";
 
 function NewEventPage() {
 
@@ -22,12 +23,18 @@ export async function action({request, params}) {
         date: data.get('date'),
         description: data.get('description'),
     }
-    fetch('http://localhost:8080/events', {
+    const response = await  fetch('http://localhost:8080/events', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(eventData),
-    })
+    });
+
+    if (!response.ok) {
+        throw json({message: 'Could not save event.'}, { status: 500})
+    }
+
+    return redirect('/events')
 
 };
