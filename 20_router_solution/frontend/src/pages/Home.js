0786a4445cda11c2ceb6ac33fb0801184598553d
neighborpil <neighborpil@naver.com>
2023-02-08 08:49:24 +0900

    save

--- 20_router_solution/frontend/src/pages/Home.js
+++ 20_router_solution/frontend/src/pages/Home.js
@@ -1,8 +1,10 @@
+import PageContent from "../components/PageContent";
+
 function HomePage() {
     return (
-        <>
-            <h1>HomePage</h1>
-        </>
+        <PageContent title={'Welcome!'}>
+            <p>Browse all our amazing event!</p>
+        </PageContent>
     );
 }
 
