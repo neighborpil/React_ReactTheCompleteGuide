dd8aee7b77c47139d73c07ab9b5cb93619f96731
neighborpil <neighborpil@naver.com>
2023-02-14 09:03:17 +0900

    save

--- 21_adding_authentication_to_react_apps/frontend/src/pages/Authentication.js
+++ 21_adding_authentication_to_react_apps/frontend/src/pages/Authentication.js
@@ -24,7 +24,7 @@ export async function action({request}) {
     const response = await fetch('http://localhost:8080/' + mode, {
         method: 'POST',
         headers: {
-            'Content-type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(authData),
     });
@@ -34,15 +34,17 @@ export async function action({request}) {
     }
 
     if (!response.ok) {
-        throw json({message: 'Could not authenticte user.'}, {status: 500});
+        throw json({message: 'Could not authenticate user.'}, {status: 500});
     }
 
     const resData = await response.json();
     const token = resData.token;
 
     localStorage.setItem('token', token);
+    const expiration = new Date();
+    expiration.setHours(expiration.getHours() + 1);
+    localStorage.setItem('expiration', expiration.toISOString());
 
-    // TODO: manage that token
     return redirect('/');
 
 
