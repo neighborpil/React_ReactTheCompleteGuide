caa256b758293146a525c032c3372a33129e53b8
neighborpil <neighborpil@naver.com>
2023-03-08 08:55:29 +0900

    save

--- 23_animations--02-modal-open-close/src/App.js
+++ 23_animations--02-modal-open-close/src/App.js
@@ -1,35 +1,72 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import {Transition} from "react-transition-group";
 
 class App extends Component {
-  state = {
-    modalIsOpen: false
-  }
+    state = {
+        modalIsOpen: false,
+        showBlock: false,
+    }
 
-  showModal = () => {
-    this.setState({modalIsOpen: true});
-  }
+    showModal = () => {
+        this.setState({modalIsOpen: true});
+    }
 
-  closeModal = () => {
-    this.setState({modalIsOpen: false});
-  }
+    closeModal = () => {
+        this.setState({modalIsOpen: false});
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.modalIsOpen} />
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
-        <h3>Animating Lists</h3>
-        <List />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>React Animations</h1>
+                <button className="Button"
+                        onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle
+                </button>
+                <br/>
+                {/*{this.state.showBlock ?*/}
+                <Transition
+                    in={this.state.showBlock}
+                    timeout={1000}
+                    mountOnEnter
+                    unmountOnExit
+                >
+                    {/*{state => <p>{state}</p>}*/}
+                    {state => (
+                        <div style={{
+                            backgroundColor: 'red',
+                            width: 100,
+                            height: 100,
+                            margin: 'auto',
+                            transition: 'opacity 1s ease-out',
+                            opacity: state === 'exiting' ? 0 : 1
+                        }}/>
+                    )}
+                </Transition>
+                {/*: null}*/}
+                <Transition
+                    mountOnEnter
+                    unmountOnExit
+                    in={this.state.modalIsOpen}
+                    timeout={300}
+                >
+                    {state => (
+                        <Modal show={state} closed={this.closeModal}/>
+                    )}
+                </Transition>
+                {/*{this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}*/}
+                {/*{this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen}/> : null}*/}
+                {this.state.modalIsOpen ? <Backdrop show /> : null}
+                <button className="Button" onClick={this.showModal}>Open Modal</button>
+                <h3>Animating Lists</h3>
+                <List/>
+            </div>
+        );
+    }
 }
 
 export default App;
